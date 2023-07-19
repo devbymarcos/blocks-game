ebd24e538d74b33d078209272cc4c1fe1bacf944
Marcos <marcoslopes.dev@gmail.com>
2023-07-19 00:21:22 -0300

    feat : remoao da tela  em beta

--- src/components/block/Block.jsx
+++ src/components/block/Block.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import style from "./style.module.css";
 
-const Block = ({ onClick }) => {
-  return <div onClick={onClick} className={style.block}></div>;
+const Block = ({ onClick, id, children }) => {
+  return (
+    <div id={id} onClick={onClick} className={style.block}>
+      {children}
+    </div>
+  );
 };
 
 export default Block;
--- src/components/main-game/MainGame.jsx
+++ src/components/main-game/MainGame.jsx
@@ -4,34 +4,78 @@ import style from "./style.module.css";
 import { useEffect, useState, useRef } from "react";
 
 function MainGame() {
-  const [blockRepeat, setBlockRepeat] = useState(Array.from({ length: 8 }));
+  const [blockRepeat, setBlockRepeat] = useState([]);
+  const [item, setItem] = useState(0);
+  const [click, setClick] = useState(1);
+  const [arrDrop, setArrDrop] = useState([]);
+
   const areaHeight = useRef();
   const mainHeight = useRef();
 
   function setBlock() {
     const area = areaHeight.current.getBoundingClientRect().bottom;
     const main = mainHeight.current.getBoundingClientRect().bottom;
+
     if (area + 50 < main) {
-      setBlockRepeat(blockRepeat.concat(Array.from({ length: 8 })));
+      setBlockRepeat((blockRepeat) => {
+        const arr = [...blockRepeat];
+        arr.unshift(item);
+        return arr;
+      });
+      setItem((item) => {
+        return item + 1;
+      });
     }
   }
 
   function handleClick(e) {
-    e.currentTarget.style.backgroundColor = "#fff";
+    const id = e.currentTarget.id;
+    console.log(id);
+    setArrDrop((arrDrop) => {
+      const arr = [...arrDrop];
+      arr.push(id);
+      return arr;
+    });
+    e.currentTarget.style.backgroundColor = "transparent";
+    setClick((click) => {
+      return click + 1;
+    });
   }
+
+  function update() {
+    if (click >= 8) {
+      const arr = [...blockRepeat];
+      console.log(arrDrop);
+      arrDrop.forEach((item) => {
+        //arr.splice(item, 1);
+        document.getElementById(item).style.display = "none";
+      });
+      setArrDrop([]);
+      setBlockRepeat(arr);
+      setClick(1);
+    }
+  }
+
+  useEffect(() => {
+    update();
+  }, [arrDrop]);
 
   useEffect(() => {
     const time = setTimeout(() => {
       setBlock();
-    }, 1000);
+    }, 3000);
     return () => clearTimeout(time);
   }, [blockRepeat]);
 
   return (
     <div ref={mainHeight} className={style.mainGame}>
       <div ref={areaHeight} className={style.areaGame}>
-        {blockRepeat.map((_, index) => {
-          return <Block onClick={handleClick} key={index} />;
+        {blockRepeat.map((bl, index) => {
+          return (
+            <Block id={index} onClick={handleClick} key={index}>
+              {""}
+            </Block>
+          );
         })}
       </div>
       <Cannon />
