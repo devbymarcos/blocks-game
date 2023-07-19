1c858ffb398e8abfd5951885462732703a474ad7
Marcos <marcoslopes.dev@gmail.com>
2023-07-19 10:08:05 -0300

    reafctor: display grid

--- src/components/block/Block.jsx
+++ src/components/block/Block.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import style from "./style.module.css";
 
 const Block = ({ onClick, id, children }) => {
+  const [click, setClick] = useState(1);
+
   return (
     <div id={id} onClick={onClick} className={style.block}>
       {children}
